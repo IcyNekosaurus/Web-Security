a1d5cf22cb9577762b6553942b85116ef2c18b10
Nekosaurus <loneice02@gmail.com>
2026-03-17 21:24:11 -0400

    Added Angular components, pages and bootstrap setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Inicio } from './pages/inicio/inicio';
+import { About } from './pages/about/about';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Inicio, About, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
